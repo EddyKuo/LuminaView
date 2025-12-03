8ae96ff54c626eea1e1122f912e43a19ad2b0400
Eddy Kuo <itsuki520@gmail.com>
2025-12-03 22:37:35 +0800

    Cleanup: Remove unnecessary .md and .bat files

--- TestWindow.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting LuminaView...
-cd src\PhotoViewer.App\bin\Debug\net8.0-windows
-PhotoViewer.App.exe
-if errorlevel 1 (
-    echo Error occurred! Error level: %errorlevel%
-    pause
-)
